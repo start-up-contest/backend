fc62c90a0da52a5abbeef5160a9ceedbc261232d
hong-sile <hyukjoon.dev@gmail.com>
2024-07-25 15:29:19 +0900

    init project

---
+++ src/main/java/cau/konnect/KonnectApplication.java
@@ -0,0 +1,13 @@
+package cau.konnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonnectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KonnectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/cau/konnect/KonnectApplicationTests.java
@@ -0,0 +1,13 @@
+package cau.konnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonnectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
